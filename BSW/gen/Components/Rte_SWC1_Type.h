ba429336345e4e3e9293c6ae829b385e65ad5f76
Colin Pan <a>
2024-09-04 09:25:32 +0800

    SWC PORT编译成功

---
+++ BSW/gen/Components/Rte_SWC1_Type.h
@@ -0,0 +1,55 @@
+/**********************************************************************************************************************
+ *  COPYRIGHT
+ *  -------------------------------------------------------------------------------------------------------------------
+ *  \verbatim
+ *
+ *                This software is copyright protected and proprietary to Vector Informatik GmbH.
+ *                Vector Informatik GmbH grants to you only those rights as set out in the license conditions.
+ *                All other rights remain with Vector Informatik GmbH.
+ *  \endverbatim
+ *  -------------------------------------------------------------------------------------------------------------------
+ *  FILE DESCRIPTION
+ *  -------------------------------------------------------------------------------------------------------------------
+ *             File:  Rte_SWC1_Type.h
+ *           Config:  TC37X_VCU.dpa
+ *      ECU-Project:  TC37X_VCU
+ *
+ *        Generator:  MICROSAR RTE Generator Version 4.26.0
+ *                    RTE Core Version 1.26.0
+ *          License:  CBD2101138
+ *
+ *      Description:  Application types header file for SW-C <SWC1>
+ *********************************************************************************************************************/
+
+/* double include prevention */
+#ifndef RTE_SWC1_TYPE_H
+# define RTE_SWC1_TYPE_H
+
+# ifdef __cplusplus
+extern "C"
+{
+# endif /* __cplusplus */
+
+# include "Rte_Type.h"
+
+# ifndef RTE_CORE
+
+/**********************************************************************************************************************
+ * Range, Invalidation, Enumeration and Bit Field Definitions
+ *********************************************************************************************************************/
+
+#  ifndef RELEASED
+#   define RELEASED (0U)
+#  endif
+
+#  ifndef REQUESTED
+#   define REQUESTED (1U)
+#  endif
+
+# endif /* RTE_CORE */
+
+# ifdef __cplusplus
+} /* extern "C" */
+# endif /* __cplusplus */
+
+#endif /* RTE_SWC1_TYPE_H */
