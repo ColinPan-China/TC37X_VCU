51cfcf4344052877ad1b96377d66d8efd38bc115
Colin Pan <a>
2025-02-19 15:54:01 +0800

    PWM标定修改

--- BSW/Modules/Xcp/XcpAppl.c
+++ BSW/Modules/Xcp/XcpAppl.c
@@ -640,11 +640,12 @@ extern volatile const uint8 Calib_Sig0[32];
 extern volatile uint8 Calib_Sig2[32];
 
 
-extern const volatile uint32 PwmOutPeriod_Set_Flash1[8];
+extern const uint32 PwmOutPeriod_Set_Flash1[8];
 extern volatile uint32 PwmOutPeriod_Set_Ram1[8];
 
-extern const volatile uint32 PwmOutPeriod_Set_Flash2[8];
-extern volatile uint32 PwmOutPeriod_Set_Ram2[8];
+
+//extern const uint32 PwmOutPeriod_Set_Flash2[8];
+//extern volatile uint32 PwmOutPeriod_Set_Ram2[8];
 
 /* Add by panjian for Enable OVC*/
 FUNC(uint8, XCP_CODE) XcpAppl_SetCalPage( uint8 Segment, uint8 Page, uint8 Mode )
@@ -680,9 +681,9 @@ FUNC(uint8, XCP_CODE) XcpAppl_SetCalPage( uint8 Segment, uint8 Page, uint8 Mode 
         IfxCpu_OverlayAddressMask_32byte, (uint32)&PwmOutPeriod_Set_Flash1,
         (uint32)&PwmOutPeriod_Set_Ram1);
 
-      IfxCpu_enableOverlayBlock(IfxCpu_ResourceCpu_0, 2, IfxCpu_OverlayMemorySelect_core0DsprPspr,
-        IfxCpu_OverlayAddressMask_32byte, (uint32)&PwmOutPeriod_Set_Flash2,
-        (uint32)&PwmOutPeriod_Set_Ram2);  
+//      IfxCpu_enableOverlayBlock(IfxCpu_ResourceCpu_0, 2, IfxCpu_OverlayMemorySelect_core0DsprPspr,
+//        IfxCpu_OverlayAddressMask_32byte, (uint32)&PwmOutPeriod_Set_Flash2,
+//        (uint32)&PwmOutPeriod_Set_Ram2);  
     }
 
     if( Page == 0x00U )
--- CDD/PwmIf/PwmIf.c
+++ CDD/PwmIf/PwmIf.c
@@ -6,18 +6,26 @@
 
 uint32 DutyTick[2] = { 0x4000, 0x4000 };
 
-const volatile uint32 PwmOutPeriod_Set_Flash1[8] = { PWM_DEFAULT_PERIOD1, PWM_DEFAULT_PERIOD1, PWM_DEFAULT_PERIOD1, PWM_DEFAULT_PERIOD1,
+
+#pragma align 32
+volatile const uint32 PwmOutPeriod_Set_Flash1[8] = { PWM_DEFAULT_PERIOD1, PWM_DEFAULT_PERIOD2, PWM_DEFAULT_PERIOD1, PWM_DEFAULT_PERIOD1,
                                                      PWM_DEFAULT_PERIOD1, PWM_DEFAULT_PERIOD1, PWM_DEFAULT_PERIOD1, PWM_DEFAULT_PERIOD1};
 
-volatile uint32 PwmOutPeriod_Set_Ram1[8] = { PWM_DEFAULT_PERIOD1+1, PWM_DEFAULT_PERIOD1+2, PWM_DEFAULT_PERIOD1+3, PWM_DEFAULT_PERIOD1+4,
-                                             PWM_DEFAULT_PERIOD1+5, PWM_DEFAULT_PERIOD1+6, PWM_DEFAULT_PERIOD1+7, PWM_DEFAULT_PERIOD1+8};
 
-const volatile uint32 PwmOutPeriod_Set_Flash2[8] = { PWM_DEFAULT_PERIOD2, PWM_DEFAULT_PERIOD2, PWM_DEFAULT_PERIOD2, PWM_DEFAULT_PERIOD2,
+volatile uint32 PwmOutPeriod_Set_Ram1[8] = { PWM_DEFAULT_PERIOD1, PWM_DEFAULT_PERIOD2, PWM_DEFAULT_PERIOD1, PWM_DEFAULT_PERIOD1,
+                                             PWM_DEFAULT_PERIOD1, PWM_DEFAULT_PERIOD1, PWM_DEFAULT_PERIOD1, PWM_DEFAULT_PERIOD1};
+#pragma align restore
+
+#if 0
+#pragma align 32
+volatile const uint32 PwmOutPeriod_Set_Flash2[8] = { PWM_DEFAULT_PERIOD2, PWM_DEFAULT_PERIOD2, PWM_DEFAULT_PERIOD2, PWM_DEFAULT_PERIOD2,
                                                      PWM_DEFAULT_PERIOD2, PWM_DEFAULT_PERIOD2, PWM_DEFAULT_PERIOD2, PWM_DEFAULT_PERIOD2};
 
 volatile uint32 PwmOutPeriod_Set_Ram2[8] = { PWM_DEFAULT_PERIOD2+1, PWM_DEFAULT_PERIOD2+2, PWM_DEFAULT_PERIOD2+3, PWM_DEFAULT_PERIOD2+4,
                                              PWM_DEFAULT_PERIOD2+5, PWM_DEFAULT_PERIOD2+6, PWM_DEFAULT_PERIOD2+7, PWM_DEFAULT_PERIOD2+8};
-                                       
+#pragma align restore
+#endif
+
 uint32 PwmOut1Period_Cur = 8000;
 uint32 PwmOut2Period_Cur = 8000;
 
@@ -41,9 +49,9 @@ void PwnIf_Main()
         PwmOut1Period_Cur = PwmOutPeriod_Set_Flash1[0];
     }
 
-    if( PwmOutPeriod_Set_Flash2[0] != PwmOut2Period_Cur )
+    if( PwmOutPeriod_Set_Flash1[1] != PwmOut2Period_Cur )
     {
-        Pwm_17_GtmCcu6_SetPeriodAndDuty(Pwm_17_GtmCcu6Conf_PwmChannel_PwmChannel_PWM_OUT2,PwmOutPeriod_Set_Flash2[0],DutyTick[1]);  
-        PwmOut2Period_Cur = PwmOutPeriod_Set_Flash2[0];
+        Pwm_17_GtmCcu6_SetPeriodAndDuty(Pwm_17_GtmCcu6Conf_PwmChannel_PwmChannel_PWM_OUT2,PwmOutPeriod_Set_Flash1[1],DutyTick[1]);  
+        PwmOut2Period_Cur = PwmOutPeriod_Set_Flash1[1];
     }   
 }
